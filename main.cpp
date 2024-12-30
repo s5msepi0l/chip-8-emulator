ab28aa3414e6b088b490fce186bdf707e2bd3e2c
jacob lindborg <65565795+s5msepi0l@users.noreply.github.com>
2024-12-31 01:03:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,15 @@
 int main(int argc, char* argv[]) {
     chip_8::cpu emulator("Chip-8 emulator");
     
-    emulator.load(
-        {
-        0x00, 0x00,
-        0x12, 0x00,
-        0x00, 0x00,
-        0xff, 0xff
-        });
+    if (argc == 2) {
+        emulator.load_program(std::string(argv[1]));
+    }
+    else {
+        std::cout << "Invalid argv[1]\n";
+        return -1;
+    }
 
+    
     emulator.run();
 
     return 0;
